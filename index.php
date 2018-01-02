e0a80ed5dc02acd1978f9b7315ab51ea848235ea
shorugby14 <shorugby14@hotmail.co.jp>
2018-01-03 07:26:46 +0900

    first

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+$app = new \Slim\App();
+$container = $app->getContainer();
+
+//ルーティングの設定
+$app->get('/',clubhitter\Controller\Top::class);
+
+$container['view'] = function($container){
+    return new \Slim\Views\Twig(__DIR__.'/templates/');
+};
+$app->run();
